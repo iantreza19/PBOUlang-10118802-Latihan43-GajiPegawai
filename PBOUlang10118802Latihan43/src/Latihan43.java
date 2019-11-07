8621f9c98630c4e522e766be5884b1f3f316c105
iantreza19 <55836534+iantreza19@users.noreply.github.com>
2019-11-07 22:37:21 +0700

    Add files via upload

---
+++ PBOUlang10118802Latihan43/src/Latihan43.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IantReza
+ */
+public class Latihan43 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GajiPegawai objG = new GajiPegawai();
+        objG.tampilData("", "", 0, 0, 0, 0);
+    }
+    
+}
